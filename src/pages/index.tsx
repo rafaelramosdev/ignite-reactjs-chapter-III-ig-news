a7f00fc7720c56308f8f5b14527e7c135e263e75
Rafael Ramos <rafaelramosdev@gmail.com>
2022-02-01 15:15:43 -0300

    feat: consuming stripe api to return the value of the product configured there on the Stripe platform on getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
+import { GetServerSideProps } from 'next'
+
 import Head from 'next/head'
+
+import { stripe } from '../services/styipe'
 
 import { SubscribeButton } from '../components/SubscribeButton'
 
 import styles from '../styles/home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -17,7 +28,7 @@ export default function Home() {
           <h1>News about <br /> the <span>React</span> world</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
           <SubscribeButton />
@@ -27,4 +38,24 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KOQRsFRUqrmGnwbG92WokxH', {
+    expand: ['product']
+  })
+  
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100), // prices are usually saved in cents in the databases, so the need to divide by 100
+  } 
+
+  return {
+    props: {
+      product
+    }
+  }
 }
