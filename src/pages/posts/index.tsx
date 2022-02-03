0d112c9ebab4400f1b69240e1460b39f86557a3b
Rafael Ramos <rafaelramosdev@gmail.com>
2022-02-03 16:40:03 -0300

    feat: create functionality to list on-screen posts consuming prismic service in 'getStaticProps'

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,27 @@
+import { GetStaticProps } from 'next'
+
 import Head from 'next/head'
+
+import Prismic from '@prismicio/client'
+
+import { RichText } from 'prismic-dom'
+
+import { getPrismicClient } from '../../services/prismic'
+
 import styles from './styles.module.scss'
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[] 
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -10,23 +30,50 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href='#'>
-            <time>12 de março de 2021</time>
-            <strong>Do back ao mobile: de onde surgiu a programação fullstack</strong>
-            <p>O  mercado da programação é bastante exigente no que se refere às novas  tecnologias e conhecimentos em termos de carreiras e experiências. A  comunidade é extremamente apaixonada no que faz e precisa,  constantemente, se manter atualizada nas tendências do mercado.</p>
-          </a>
-          <a href='#'>
-            <time>12 de março de 2021</time>
-            <strong>Do back ao mobile: de onde surgiu a programação fullstack</strong>
-            <p>O  mercado da programação é bastante exigente no que se refere às novas  tecnologias e conhecimentos em termos de carreiras e experiências. A  comunidade é extremamente apaixonada no que faz e precisa,  constantemente, se manter atualizada nas tendências do mercado.</p>
-          </a>
-          <a href='#'>
-            <time>12 de março de 2021</time>
-            <strong>Do back ao mobile: de onde surgiu a programação fullstack</strong>
-            <p>O  mercado da programação é bastante exigente no que se refere às novas  tecnologias e conhecimentos em termos de carreiras e experiências. A  comunidade é extremamente apaixonada no que faz e precisa,  constantemente, se manter atualizada nas tendências do mercado.</p>
-          </a>
+          { posts.map(post => {
+            return (
+              <a key={post.slug} href='#' >
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            )
+          }) }
         </div>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query<any>(
+    [Prismic.predicates.at("document.type", "post")],
+    {
+      fetch: ["post.title", "post.content"],
+      pageSize: 100,
+    }
+  );
+
+  // console.log(JSON.stringify(response, null, 2)) // whenever you give the 'console.log' on some object or array that has some cascading internal content (example: 'date: [Object]' | 'slugs: [Array]'), do it this way
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
