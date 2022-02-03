bf1fa2061fdff596cc0b98a6d196d676a585de61
Rafael Ramos <rafaelramosdev@gmail.com>
2022-02-03 16:04:36 -0300

    feat: create prismic service

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -2,7 +2,7 @@ import Prismic from '@prismicio/client'
 
 export function getPrismicClient(request?: unknown) {
   const prismic = Prismic.client(
-    process.env.PRISMIC_ACCESS_TOKEN,
+    process.env.PRISMIC_ENDPOINT,
     {
       req: request,
       accessToken: process.env.PRISMIC_ACCESS_TOKEN
